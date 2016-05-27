a07810ed63b13747091fbc125021243a2cdd4c39
Holger Wolff <holger.wolff@monash.edu>
2016-05-27 15:46:23 +1000

    passing hwody_test, failing previous

--- greeter_mod.f90
+++ greeter_mod.f90
@@ -6,11 +6,17 @@ module greeter_mod
             character(len=*), intent(in), optional :: cAddressee, cGreeting
             character(len=50) :: greeter
             character(len=30) :: myAddressee
+            character(len=20) :: myGreeting
+            if (present(cGreeting)) then
+                myGreeting = cGreeting
+            else
+                myGreeting = "hello"
+            end if
             if (present(cAddressee)) then
                 myAddressee = cAddressee
             else
                 myAddressee = "World"
             end if
-            greeter = "Hello " // trim(myAddressee)
+            greeter = trim(myGreeting) // " " // trim(myAddressee)
         end function greeter
 end module greeter_mod
