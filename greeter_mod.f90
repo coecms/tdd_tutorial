7ee23e7a0bb5ae3977b36ecec7c4840ce474a8b0
Holger Wolff <holger.wolff@monash.edu>
2016-05-27 15:11:14 +1000

    added optional cAddressee parameter

--- greeter_mod.f90
+++ greeter_mod.f90
@@ -1,8 +1,9 @@
 module greeter_mod
     implicit none
     contains
-        function greeter()
+        function greeter(cAddressee)
             implicit none
+            character(len=*), intent(in), optional :: cAddressee
             character(len=50) :: greeter
             greeter = "Hello World"
         end function greeter
