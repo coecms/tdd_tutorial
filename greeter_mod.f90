2116bda46da23eeb3ed3b0884124bdb0b85b3c77
Holger Wolff <holger.wolff@monash.edu>
2016-05-27 15:13:12 +1000

    made greeter pass test_pluto

--- greeter_mod.f90
+++ greeter_mod.f90
@@ -5,6 +5,12 @@ module greeter_mod
             implicit none
             character(len=*), intent(in), optional :: cAddressee
             character(len=50) :: greeter
-            greeter = "Hello World"
+            character(len=30) :: myAddressee
+            if (present(cAddressee)) then
+                myAddressee = cAddressee
+            else
+                myAddressee = "World"
+            end if
+            greeter = "Hello " // trim(myAddressee)
         end function greeter
 end module greeter_mod
